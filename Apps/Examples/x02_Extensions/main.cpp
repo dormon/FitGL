3c29887e22bb205deca554ec3be65462ac81720d
Neithy <neithy.cz@gmail.com>
2016-11-03 11:13:53 +0100

    Extensions

---
+++ Apps/Examples/x02_Extensions/main.cpp
@@ -0,0 +1,17 @@
+#include <BaseApp.h>
+#include <string>
+
+int main(int /*argc*/, char ** /*argv*/) {
+	BaseApp app;
+	
+  app.addInitCallback([&] {
+    int count;
+    glGetIntegerv(GL_NUM_EXTENSIONS, &count);
+    for (int i = 0; i < count; i++) {
+      auto a = glGetStringi(GL_EXTENSIONS, i);
+      std::cout << a << "\n";
+    }
+  });
+
+	return app.run();
+}
